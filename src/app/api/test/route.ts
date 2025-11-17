e85995c82b129bebf01e7461dc64f4631b058149
indepadib <55211714+indepadib@users.noreply.github.com>
2025-11-17 22:04:29 +0100

    Change response method to use NextResponse

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,6 +1,7 @@
-import { prisma } from 'src/app/lib/prisma.ts';
+import { NextResponse } from 'next/server';
+import { prisma } from '../../lib/prisma';
 
 export async function GET() {
   const merchants = await prisma.merchant.findMany();
-  return Response.json({ ok: true, merchants });
+  return NextResponse.json({ ok: true, merchants });
 }
