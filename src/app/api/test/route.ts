fdf8cace867641de8ee99f9363bfaff1d543d307
indepadib <55211714+indepadib@users.noreply.github.com>
2025-11-17 21:38:07 +0100

    Implement GET route to fetch merchants

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,6 @@
+import { prisma } from "@/src/lib/prisma";
+
+export async function GET() {
+  const merchants = await prisma.merchant.findMany();
+  return Response.json({ ok: true, merchants });
+}
